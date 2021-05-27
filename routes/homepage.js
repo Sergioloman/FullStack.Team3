fd24799c44bacc9ce89f1a1fc092ad4c07efad44
kwinahn <77373020+kwinahn@users.noreply.github.com>
2021-05-26 21:03:03 -0500

    homepage hbs with working routes

--- routes/homepage.js
+++ routes/homepage.js
@@ -3,6 +3,26 @@ const sequelize = require('../config/connection');
 const {Prompt, User, Score } = require('../models');
 // include a login section for users to log in.
 
+router.get('/', (req, res) => {
+  Prompt.findAll({
+    attributes: [
+      'title']
+    })
+    .then(dbPrompt => {
+      //const prompts = dbPrompt.map(prompts.get({ plain: true}));
+
+      res.render('homepage', {
+        loggedIn: req.session.loggedIn
+      })
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+    
+})
+
+
 router.get('/login', (req, res) => {
   if (req.session.loggedIn) {
     res.redirect('/');

3d759f4db00ed76b989bb32e649c85b148291d62
Sergio Torres <sergio.lo.torres@gmail.com>
2021-05-29 16:06:08 -0500

    random promp generator  function works

--- public/javascript/gameLogic.js
+++ public/javascript/gameLogic.js
@@ -1,3 +1,25 @@
+//generate a random prompt
+async function randomizer(){
+    let randomPrompt = await fetch('/api/prompts/')
+        .then(data =>data.json())
+        .then(response=>{
+            let result = response.length
+            let randoresult = Math.floor(Math.random()*result)
+            return randoresult;
+        })
+    
+    console.log(randomPrompt)
+
+    fetch('/api/prompts/'+ randomPrompt)
+        .then(data =>data.json())
+        .then(response=>{
+        let result = response.title
+        console.log(result)
+        let randomPromptContainer = document.querySelector("#randomPrompt");
+        randomPromptContainer.append(result);
+        })
+
+}
 
 //cant prevent defaul efficiently
 async function addKarp(e){
@@ -10,10 +32,13 @@ async function addKarp(e){
     .then(data=>data.json())
     .then(response=>response.score)
     
+    //promptid should match the random prompt being generated.
     const promptid = 1
+
+
     const promptScore =  await fetch('api/prompts/'+ promptid)
     .then(response => response.json())
-    .then(data=>console.log(data))
+    .then(data=>console.log(data.promptScore, data.title, data.promptId))
 
     // make another api route to update the current value of player score
     finalScore = userScore + promptScore
@@ -22,29 +47,12 @@ async function addKarp(e){
     return finalScore;
 }
 
-
 function goBack(e){
     e.preventDefault()
-    console.log('no points! time to go back and play again')
+    console.log('take me back to homepage')
+    document.location.replace('/')
 }
 
-function randomizer(){
-        const index = [1,2,3,4,5,6,7,8,9,10]
-        const randomPromptId =  index[Math.floor(Math.random()*index.length)]
-        
-        const response = fetch('/api/prompts/'+ randomPromptId)
-        .then(data =>data.json())
-        .then(response=>{
-            let result = response.title
-            console.log(result)
-
-            let randomPromptContainer = document.querySelector("#randomPrompt");
-         
-            randomPromptContainer.append(result);
-            
-        })
-
-}
 
 fetch('/test',{
     method: 'post',
@@ -53,6 +61,6 @@ fetch('/test',{
 })
 
 randomizer()
-
+// setArray()  
 document.querySelector('#yesbtn').addEventListener("click",addKarp)
 document.querySelector('#nobtn').addEventListener("click",goBack)
